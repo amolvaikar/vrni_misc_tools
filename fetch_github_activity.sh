e298ead324003263a3fe006c701062e78963f403
avaikar <avaikar@vmware.com>
2025-02-14 22:17:38 +0100

    script for git activity

--- fetch_github_activity.sh
+++ fetch_github_activity.sh
@@ -16,8 +16,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Get repository details and date range
-read -p "Enter the repository owner: " REPO_OWNER
-read -p "Enter the repository name: " REPO_NAME
+REPO_OWNER="vcf"
 read -p "Enter the start date (YYYY-MM-DD): " START_DATE
 read -p "Enter the end date (YYYY-MM-DD): " END_DATE
 read -p "Enter the file containing the list of usernames (one per line): " USER_FILE
@@ -30,12 +29,15 @@ fi
 
 # Initialize summary file
 SUMMARY_FILE="summary_$START_DATE_$END_DATE.csv"
-echo "Username,Total Commits,Total PRs,Total Reviews" > "$SUMMARY_FILE"
+SUMMARY_DIR="$END_DATE-Data"
+mkdir "$SUMMARY_DIR"
+echo "Userid,Username,RepoName,Total Commits,Total PRs,Total Reviews" > "$SUMMARY_FILE"
 
 # Function to fetch commits
 fetch_commits() {
     local username=$1
-    local output_file="$username-commits.csv"
+    local REPO_NAME=$2
+    local output_file="$SUMMARY_DIR/$username-$REPO_NAME-commits.csv"
     echo >&2 "Fetching commits created by $username from $START_DATE to $END_DATE..."
     gh search commits "author-date:${START_DATE}..${END_DATE}" --repo "$REPO_OWNER/$REPO_NAME" --limit 1000 --author "$username"  --json id,committer,url --jq '.[] | [.id, .committer.login, .url] | @csv' > "$output_file"
     echo >&2 "$username commits saved to $output_file"
@@ -46,7 +48,8 @@ fetch_commits() {
 # Function to fetch pull requests opened by the user
 fetch_pull_requests() {
     local username=$1
-    local output_file="$username-pull_requests.csv"
+    local REPO_NAME=$2
+    local output_file="$SUMMARY_DIR/$username-$REPO_NAME-pull_requests.csv"
     echo  >&2 "Fetching pull requests created by $username from $START_DATE to $END_DATE..."
     gh pr list --repo "$REPO_OWNER/$REPO_NAME" --author "$username" --limit 1000 --state all --search "created:${START_DATE}..${END_DATE}" --json id,title,createdAt,author,url --jq '.[] | [.id, .title, .createdAt, .author.login, .author.name, .url] | @csv' > "$output_file"
     echo >&2  "$username pull requests saved to $output_file"
@@ -57,7 +60,8 @@ fetch_pull_requests() {
 # Function to fetch pull requests on which the user happens to be a reviewer
 fetch_review_requests() {
     local username=$1
-    local output_file="$username-review_requests.csv"
+    local REPO_NAME=$2
+    local output_file="$SUMMARY_DIR/$username-$REPO_NAME-review_requests.csv"
     echo  >&2 "Fetching pull requests reviewed by $username from $START_DATE to $END_DATE..."
     gh search prs --repo "$REPO_OWNER/$REPO_NAME" --limit 1000 "created:${START_DATE}..${END_DATE}"  --reviewed-by "$username" --json id,title,createdAt,author,url --jq '.[] | [.id, .title, .createdAt, .author.login, .author.name, .url] | @csv' | grep -v $username  > "$output_file"
     echo >&2  "$username reviewe requests saved to $output_file"
@@ -66,23 +70,29 @@ fetch_review_requests() {
 }
 
 # Process each username from the file
-while IFS= read -r USERNAME; do
-    echo "Processing data for $USERNAME..."
+while IFS= read -r USERLINE; do
+  # ToDo: add validation for the line we read from the file
+  USERID=$(echo $USERLINE | cut -d ';' -f 1)
+  USERNAME=$(echo $USERLINE | cut -d ';' -f 2)
+  for REPO_NAME in netops-main netops-ui netops-operations ; do
+    echo "Processing data for $USERID and repo $REPO_NAME..."
 
     # Fetch commits, PRs, and reviews for the user
-    COMMITS_COUNT=$(fetch_commits "$USERNAME")
-    echo "Got commits count $COMMITS_COUNT"
+    COMMITS_COUNT=$(fetch_commits "$USERID" "$REPO_NAME")
+    echo "For repo $REPO_NAME got commits count $COMMITS_COUNT"
 
-    PRS_COUNT=$(fetch_pull_requests "$USERNAME")
-    echo "Got PRs count $PRS_COUNT"
+    PRS_COUNT=$(fetch_pull_requests "$USERID" "$REPO_NAME")
+    echo "For repo $REPO_NAME got PRs count $PRS_COUNT"
 
-    REVIEWS_COUNT=$(fetch_review_requests "$USERNAME")
-    echo "Got review PR count $REVIEWS_COUNT"
+    REVIEWS_COUNT=$(fetch_review_requests "$USERID" "$REPO_NAME")
+    echo "For repo $REPO_NAME got review PR count $REVIEWS_COUNT"
 
     # Write summary to the summary.csv
-    echo "$USERNAME,$COMMITS_COUNT,$PRS_COUNT,$REVIEWS_COUNT" >> "$SUMMARY_FILE"
+    echo "$USERID,$USERNAME,$REPO_NAME,$COMMITS_COUNT,$PRS_COUNT,$REVIEWS_COUNT" >> "$SUMMARY_FILE"
+  done
 done < "$USER_FILE"
 
 echo "Summary saved to $SUMMARY_FILE"
+tar zcvf "$SUMMARY_DIR.tgz" "$SUMMARY_DIR/"
 echo "Activity data for each user has been saved to individual CSV files."
 
